077b10bef2e53d3103315164c38568f993d206d6
amorin <alexis.morin@webhelp.com>
2022-06-17 10:24:45 +0200

    feat(tests): Add test for CheckController

---
+++ src/Controller/CheckController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CheckController extends AbstractController
+{
+    /**
+     * @Route("/check", name="app_check")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/CheckController.php',
+        ]);
+    }
+}
