f5462b5805f8f518f31b60d661cd4c67df640494
amorin <alexis.morin@webhelp.com>
2022-06-17 11:20:54 +0200

    chore(ci): Test if i can merge when tests fails

--- tests/Controller/CheckControllerTest.php
+++ tests/Controller/CheckControllerTest.php
@@ -6,11 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CheckControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testIndexSuccess()
     {
         $client = static::createClient();
 
         $client->request('GET', '/check');
         $this->assertResponseIsSuccessful();
+    }
+
+    public function testIndexFail()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/check');
+        $this->assertResponseIsUnprocessable();
     }
 }
