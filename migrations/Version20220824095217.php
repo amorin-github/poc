5a2fe461ec8ba45c7ae0bbab726eba7df436e36c
amorin <alexis.morin@webhelp.com>
2022-08-24 14:19:31 +0200

    fix migration

--- migrations/Version20220824095217.php
+++ migrations/Version20220824095217.php
@@ -22,7 +22,6 @@ final class Version20220824095217 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE product_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE product (id INT NOT NULL, name VARCHAR(255) NOT NULL, price INT NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE SEQUENCE product_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
     }
 
     public function down(Schema $schema): void
